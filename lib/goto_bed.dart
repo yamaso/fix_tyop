0fed456c0c1aa94aad352e9779657c7cf2ec42f0
sota.yamamoto <yamasou23@gmail.com>
2021-09-12 01:33:20 +0900

    背景とマージ

--- lib/goto_bed.dart
+++ lib/goto_bed.dart
@@ -1,3 +1,4 @@
+import 'package:fix_tyop/services/background.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 
@@ -28,63 +29,65 @@ class _GoToSleepPageState extends State<GoToSleepPage>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       backgroundColor: Colors.deepPurpleAccent,
-      body: Row(
+      body: Stack(
         children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.end,
+          getBackgroud(),
+          Row(
             children: [
-              Text(
-                time.hour.toString() + '：' + time.minute.toString(),
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 50,
-                  fontWeight: FontWeight.w800,
-                ),
-              ),
-              SizedBox(height: 100.0),
-              ElevatedButton(
-                child: Container(
-                  width: 150,
-                  height: 150,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      fit: BoxFit.fill,
-                      image: AssetImage("images/sleep_button.png"),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    time.hour.toString() + '：' + time.minute.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.w800,
                     ),
                   ),
-                ),
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<CircleBorder>(
-                    CircleBorder(),
+                  SizedBox(height: 100.0),
+                  ElevatedButton(
+                    child: Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: AssetImage("images/sleep_button.png"),
+                        ),
+                      ),
+                    ),
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<CircleBorder>(
+                        CircleBorder(),
+                      ),
+                    ),
+                    onPressed: () {},
                   ),
-                ),
-                onPressed: () {},
+                  SizedBox(height: 100.0),
+                ],
               ),
-              SizedBox(height: 100.0),
-            ],
-          ),
-          SizedBox(width: 40),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ScaleTransition(
-                scale: animationController.drive(
-                  Tween<double>(
-                    begin: 1.0,
-                    end: 1.05,
+              SizedBox(width: 40),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ScaleTransition(
+                    scale: animationController.drive(
+                      Tween<double>(
+                        begin: 1.0,
+                        end: 1.05,
+                      ),
+                    ),
+                    child: Image.asset(
+                      'images/aka_a.png',
+                      width: 150,
+                      height: 480,
+                      fit: BoxFit.cover,
+                    ),
                   ),
-                ),
-                child: Image.asset(
-                  'images/aka_a.png',
-                  width: 150,
-                  height: 480,
-                  fit: BoxFit.cover,
-                ),
+                ],
               ),
             ],
           ),
